f1fe53da4235de0bb8870837d999cdf7e79e545b
LuisRamirez2328 <221260@ids.upchiapas.edu.mx>
2023-07-23 15:54:03 -0500

    "Listo"

--- src/componentes/Molecula/History.jsx
+++ src/componentes/Molecula/History.jsx
@@ -13,12 +13,12 @@ const History = () => {
     { fecha: '2023-07-22', temperatura: 29, descripcion: 'Soleado' },
     { fecha: '2023-07-23', temperatura: 15, descripcion: 'Casi soleado' },
     { fecha: '2023-07-22', temperatura: 5, descripcion: 'Nublado' },
-    // Agrega más datos para fechas futuras aquí...
+   
   ];
 
   return (
     <>
-      {/* Mostrar el botón si showTable es falso, de lo contrario mostrar la tabla */}
+     
       {!showTable ? (
         <Button onClick={handleButtonClick}>Historial</Button>
       ) : (
